f305240ba82c8c7b32a81c2b6868b440228ab48f
Jose Anselmi <joseanselmi27@gmail.com>
2026-02-23 19:53:16 +0100

    Fix API_URL for prod

--- gestion-clientes-app/lib/api.ts
+++ gestion-clientes-app/lib/api.ts
@@ -1,4 +1,6 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000"
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  (process.env.NODE_ENV === "development" ? "http://localhost:4000" : "")
 
 export interface Cliente {
   id: string
